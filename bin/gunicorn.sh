3283d7d395ecd6eee8857154e2e2d216ac5eb955
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2019-03-15 11:29:26 +0000

    Configure Standalone scripts to run production api

---
+++ bin/gunicorn.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+### BEGIN INIT INFO
+# Provides:          nginx
+# Required-Start:    $all
+# Required-Stop:     $all
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: starts the nginx web server
+# Description:       starts nginx using start-stop-daemon
+### END INIT INFO
+
+SCRIPT=$(readlink -f $0)
+SCRIPT_PATH=`dirname ${SCRIPT}`
+APP_PATH=`dirname ${SCRIPT_PATH}`
+PATH=$PATH:$SCRIPT_PATH:$APP_PATH
+
+DAEMON=`command -v gunicorn`
+
+NAME=${SCRIPT_PATH}/gunicorn
+DESC="Production DB Service"
+
+DAEMON_OPTS="-c $SCRIPT_PATH/gunicorn.conf.py ensembl_production_api.wsgi:application "
+
+PYTHONPATH=${PYTHONPATH}:${SCRIPT_PATH}/../
+export PYTHONPATH
+
+command -v gunicorn > /dev/null 2>&1 || { echo >&2 "no gunicorn available"; exit 1; }
+
+cd ${SCRIPT_PATH}/../
+
+function init_django() {
+    echo "Check Django Updates / Upgrades"
+    python manage.py flush --no-input
+    python manage.py makemigrations
+    python manage.py migrate
+    python manage.py collectstatic --no-input
+}
+
+set -e
+
+case "$1" in
+  start)
+    echo "Starting $DESC"
+    init_django
+    ${DAEMON} ${DAEMON_OPTS}
+    echo "$NAME."
+    ;;
+  stop)
+    echo -n "Stopping $DESC: "
+    kill -9 `ps aux | grep gunicorn | grep ensembl_production_api | awk '{ print $2 }'`
+    # start-stop-daemon --stop --quiet --pidfile ${NAME}.pid --exec ${DAEMON}  -- ${DAEMON_OPTS}
+    echo "$NAME."
+    ;;
+  restart|force-reload)
+    echo -n "Restarting $DESC: "
+    start-stop-daemon --stop --quiet --pidfile ${NAME}.pid --exec ${DAEMON}  -- ${DAEMON_OPTS}
+    sleep 1
+    init_django
+    start-stop-daemon --start --quiet --pidfile ${NAME}.pid --exec ${DAEMON} -- ${DAEMON_OPTS}
+    echo "$NAME."
+    ;;
+  reload)
+    echo -n "Reloading $DESC configuration: "
+    start-stop-daemon --stop --signal HUP --quiet --pidfile ${NAME}.pid --exec ${DAEMON}
+    echo "$NAME."
+    ;;
+  *)
+    #N=/etc/init.d/$NAME
+    echo "Usage: $N {start|stop|restart|reload|force-reload}" >&2
+    exit 1
+    ;;
+esac
+
+exit 0
