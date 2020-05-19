13571f0744a7276e79a78f752bfac63f3a12442a
Thomas Maurel <maurel@ebi.ac.uk>
2020-05-19 15:41:42 +0100

    Making job_id a read only field

--- ensembl_dbcopy/api/serializers.py
+++ ensembl_dbcopy/api/serializers.py
@@ -91,6 +91,7 @@ class RequestJobListSerializer(serializers.HyperlinkedModelSerializer):
             'end_date',
             'user',
             'overall_status')
+        read_only_fields = ['job_id']
         extra_kwargs = {
             'url': {'view_name': 'requestjob-detail', 'lookup_field': 'job_id'},
         }
@@ -119,6 +120,7 @@ class RequestJobDetailSerializer(BaseUserTimestampSerializer):
             'user',
             'overall_status',
             'detailed_status')
+        read_only_fields = ['job_id']
 
     transfer_log = TransferLogSerializer(many=True, source='transfer_logs', read_only=True)
 
