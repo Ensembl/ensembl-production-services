4fe1636952e2f5906d09890551d847898f0ee1c8
Luca Da Rin Fioretto <ldrf@ebi.ac.uk>
2020-11-26 14:50:50 +0000

    Use custom validators in dbcopy forms

--- ensembl_dbcopy/forms.py
+++ ensembl_dbcopy/forms.py
@@ -13,13 +13,22 @@
 """
 import re
 
+import logging
+
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django import forms
+from django.core.validators import RegexValidator, EmailValidator
 import sqlalchemy as sa
 
 from ensembl_dbcopy.models import RequestJob, Group, Host
 from ensembl_dbcopy.api.views import get_database_set #, get_engine
+
+
+logger = logging.getLogger(__name__)
+
+
+COMMA_RE = re.compile(',+')
 
 
 def _text_field_as_set(text):
@@ -34,123 +43,100 @@ def _apply_db_names_filter(db_names, all_db_names):
     return db_names
 
 
+class TrimmedCharField(forms.CharField):
+    def to_python(self, value):
+        value = super().to_python(value)
+        if not value:
+            return ''
+        return COMMA_RE.sub(',', value.replace(' ', '').rstrip(','))
+
+
+class ListFieldRegexValidator(RegexValidator):
+    def __call__(self, value):
+        elements = value.split(',')
+        for element in elements:
+            super().__call__(element)
+
+
+class EmailListFieldValidator(EmailValidator):
+    def __call__(self, value):
+        elements = value.split(',')
+        for element in elements:
+            super().__call__(element)
+
+
 class SubmitForm(forms.ModelForm):
     class Meta:
         model = RequestJob
         exclude = ('job_id', 'tgt_directory')
 
-    src_host = forms.CharField(
+    src_host = TrimmedCharField(
         label="Source Host (e.g: host:port)",
         max_length=2048,
-        required=True)
-    tgt_host = forms.CharField(
+        required=True,
+        validators=[
+            RegexValidator(
+                regex="^[\w-]+:[0-9]{4}",
+                message="Source Host should be formatted like this host:port"
+            )
+        ])
+    tgt_host = TrimmedCharField(
         label="Target Hosts (e.g: Host1:port1,Host2:port2)",
         max_length=2048,
-        required=True)
-    src_incl_db = forms.CharField(
+        required=True,
+        validators=[
+            ListFieldRegexValidator(
+                regex="^[\w-]+:[0-9]{4}",
+                message="Target Hosts should be formatted like this host:port or host1:port1,host2:port2"
+            )
+        ])
+    src_incl_db = TrimmedCharField(
         label="Databases to copy (e.g: db1,db2,.. or %variation_99% )",
         max_length=2048,
         required=False)
-    src_skip_db = forms.CharField(
+    src_skip_db = TrimmedCharField(
         label="Databases to exclude (e.g db1,db2 or %mart%)",
         max_length=2048,
         required=False)
-    src_incl_tables = forms.CharField(
+    src_incl_tables = TrimmedCharField(
         label="Only Copy these tables (e.g: table1,table2,..):",
         max_length=2048,
         required=False)
-    src_skip_tables = forms.CharField(
+    src_skip_tables = TrimmedCharField(
         label="Skip these tables (e.g: table1,table2)",
         max_length=2048,
         required=False)
-    tgt_db_name = forms.CharField(
+    tgt_db_name = TrimmedCharField(
         label="Name of databases on Target Hosts (e.g: db1,db2)",
         max_length=2048,
         required=False)
-    email_list = forms.CharField(
+    email_list = TrimmedCharField(
         label="Email list",
         max_length=2048,
-        required=True)
+        required=True,
+        validators=[
+            EmailListFieldValidator(
+                message="Email list should contain one or more comma separated valid email addresses."
+            )
+        ])
     user = forms.CharField(
         widget=forms.HiddenInput())
 
-    def _cleaned_text_field(self, field_name):
-        return self.cleaned_data.get(field_name, '').replace(' ', '').rstrip(',')
-
-    def clean_src_host(self):
-        src_host_pattern = re.compile("^.+:[0-9]{4}")
-        data = self._cleaned_text_field('src_host')
-        if not src_host_pattern.fullmatch(data):
-            raise forms.ValidationError("Source Host should be formatted like this host:port")
-        return data
-
-    def clean_tgt_host(self):
-        tgt_host_pattern = re.compile("^(.+:[0-9]{4},?)+")
-        data = self._cleaned_text_field('tgt_host')
-        if not tgt_host_pattern.fullmatch(data):
-            raise forms.ValidationError(
-                "Source Host should be formatted like this host:port or host1:port1,host2:port2")
-        # Checking that user is allowed to copy to the target server.
-        for host in self._cleaned_text_field('tgt_host').split(','):
-            cleaned_host = host.split(':')[0]
-            host_queryset = Host.objects.filter(name=cleaned_host)
-            group = Group.objects.filter(host_id=host_queryset[0].auto_id)
-            if group:
-                host_groups = group.values_list('group_name', flat=True)
-                user_groups = self.user.groups.values_list('name', flat=True)
-                common_groups = set(host_groups).intersection(set(user_groups))
-                if not common_groups:
-                    raise forms.ValidationError("You are not allowed to copy to " + cleaned_host)
-        return data
-
-    def clean_src_incl_db(self):
-        return self._cleaned_text_field('src_incl_db')
-
-    def clean_src_skip_db(self):
-        src_skip_db = self._cleaned_text_field('src_skip_db')
-        tgt_db_name = self._cleaned_text_field('tgt_db_name')
-        src_skip_db_names = _text_field_as_set(src_skip_db)
-        tgt_db_names = _text_field_as_set(tgt_db_name)
+    def _validate_db_skipping(self, src_skip_db_names, tgt_db_names):
         if src_skip_db_names and tgt_db_names:
-            raise forms.ValidationError(
+            self.add_error('src_skip_db',
                     'Field "Names of databases on Target Host" is not empty. Consider clear it, or clear this field.')
-        return src_skip_db
 
-    def clean_src_incl_tables(self):
-        return self._cleaned_text_field('src_incl_tables')
-
-    def clean_src_skip_tables(self):
-        return self._cleaned_text_field('src_skip_tables')
-
-    def clean_email_list(self):
-        email_list_pattern = re.compile("^(.+@.+,?)+")
-        data = self._cleaned_text_field('email_list')
-        if not email_list_pattern.fullmatch(data):
-            raise forms.ValidationError(
-                "Email list should be formatted like this joe.bloggs@ebi.ac.uk or joe.bloggs@ebi.ac.uk,toto@ebi.ac.uk")
-        return data
-
-    def clean_tgt_db_name(self):
-        tgt_db_name = self._cleaned_text_field('tgt_db_name')
-        src_incl_db = self._cleaned_text_field('src_incl_db')
-        tgt_dbs = _text_field_as_set(tgt_db_name)
-        src_dbs = _text_field_as_set(src_incl_db)
-        if tgt_db_name:
+    def _validate_db_renaming(self, src_dbs, tgt_dbs):
+        if tgt_dbs:
             if len(tgt_dbs) != len(src_dbs):
-                raise forms.ValidationError(
+                self.add_error('tgt_db_name',
                     "The number of databases to copy should match the number of databases renamed on target hosts")
-            if '%' in src_incl_db:
-                raise forms.ValidationError(
-                    "You can't rename a pattern")
-        return tgt_db_name
+            for dbname in src_dbs:
+                if '%' in dbname:
+                    self.add_error('tgt_db_name', "You can't rename a pattern")
 
-    def clean(self):
-        cleaned_data = super().clean()
-        src_host = cleaned_data.get('src_host')
-        tgt_hosts = _text_field_as_set(cleaned_data.get('tgt_host'))
-        src_dbs = _text_field_as_set(cleaned_data.get('src_incl_db'))
-        src_skip_dbs = _text_field_as_set(cleaned_data.get('src_skip_db'))
-        tgt_db_names = _text_field_as_set(cleaned_data.get('tgt_db_name'))
+    def _validate_source_and_target(self, src_host, tgt_hosts, src_dbs, src_skip_dbs, tgt_db_names):
         if src_host in tgt_hosts:
             hostname, port = src_host.split(':')
             try:
@@ -168,17 +154,11 @@ class SubmitForm(forms.ModelForm):
                 if db_names:
                     raise forms.ValidationError('Some source and target databases coincide. Please add target names or change sources')
 
-    ## Commented until this feature is enabled by DBAs
-    #  def clean_wipe_target(self):
-    #      wipe_target = self.cleaned_data.get('wipe_target')
-    #      src_dbs = self._text_field_as_set(self._cleaned_text_field('src_incl_db'))
-    #      src_skip_dbs = self._text_field_as_set(self._cleaned_text_field('src_skip_db'))
+    #  # Commented until this feature is enabled by DBAs
+    #  def _validate_wipe_target(self, wipe_target, src_dbs, src_skip_dbs, tgt_hosts, tgt_db_names, src_incl_tables):
     #      src_db_names = src_dbs.difference(src_skip_dbs)
-    #      tgt_db_names = self._text_field_as_set(self._cleaned_text_field('tgt_db_name'))
     #      new_db_names = tgt_db_names if tgt_db_names else src_db_names
-    #      src_incl_tables = self._cleaned_text_field('src_incl_tables')
     #      if (wipe_target is False) and (not src_incl_tables) and new_db_names:
-    #          tgt_hosts = self._text_field_as_set(self._cleaned_text_field('tgt_host'))
     #          for tgt_host in tgt_hosts:
     #              hostname, port = tgt_host.split(':')
     #              try:
@@ -192,7 +172,34 @@ class SubmitForm(forms.ModelForm):
     #                  self.add_error(field_name,
     #                                 'One or more database names already present on the target. Consider enabling Wipe target option.')
     #                  break
-    #      return wipe_target
+
+    def _validate_user_permission(self, tgt_hosts):
+        # Checking that user is allowed to copy to the target server.
+        for host in tgt_hosts:
+            cleaned_host = host.split(':')[0]
+            host_queryset = Host.objects.filter(name=cleaned_host)
+            group = Group.objects.filter(host_id=host_queryset[0].auto_id)
+            if group:
+                host_groups = group.values_list('group_name', flat=True)
+                user_groups = self.user.groups.values_list('name', flat=True)
+                common_groups = set(host_groups).intersection(set(user_groups))
+                if not common_groups:
+                    self.add_error('tgt_host', "You are not allowed to copy to " + cleaned_host)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        src_host = cleaned_data['src_host']
+        # wipe_target = cleaned_data['wipe_target']
+        src_incl_tables = cleaned_data['src_incl_tables']
+        tgt_hosts = _text_field_as_set(cleaned_data['tgt_host'])
+        src_dbs = _text_field_as_set(cleaned_data['src_incl_db'])
+        src_skip_dbs = _text_field_as_set(cleaned_data['src_skip_db'])
+        tgt_db_names = _text_field_as_set(cleaned_data['tgt_db_name'])
+        self._validate_db_skipping(src_skip_dbs, tgt_db_names)
+        self._validate_db_renaming(src_dbs, tgt_db_names)
+        self._validate_source_and_target(src_host, tgt_hosts, src_dbs, src_skip_dbs, tgt_db_names)
+        # self._validate_wipe_target(wipe_target, src_dbs, src_skip_dbs, tgt_hosts, tgt_db_names, src_incl_tables)
+        self._validate_user_permission(tgt_hosts)
 
     def __init__(self, *args, **kwargs):
         if 'initial' in kwargs and 'from_request_job' in kwargs['initial']:
